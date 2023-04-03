e600360891fcfcc759756012eff136f0aa6f9700
Daan Braas <daanbraas@gmail.com>
2023-04-03 19:18:22 +0200

    Opdracht 3 #4

--- relational-data-storage/opdracht-3/DS-opdracht-3.2.10.sql
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.10.sql
@@ -1,3 +1,3 @@
 --3.2.10 Selecteer naam, straat, huisnummer en postcode van alle leveranciers wiens naam HTML-entities bevat (&xxxx;).
 SELECT sup.name, straat, huisnr, postcode FROM mhl_suppliers sup
-WHERE name LIKE '%&____;%'
+WHERE name LIKE '%&__%__;%'

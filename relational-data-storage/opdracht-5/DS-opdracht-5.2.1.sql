cbc1c45966b0e64cc44e4c8ed0b64b8eb602f7c8
Daan Braas <daanbraas@gmail.com>
2023-04-04 09:52:39 +0200

    Opdracht 5.2 #6

--- relational-data-storage/opdracht-5/DS-opdracht-5.2.1.sql
+++ relational-data-storage/opdracht-5/DS-opdracht-5.2.1.sql
@@ -2,17 +2,16 @@
 --gesorteerd per provincie, plaatsnaam en naam: 
 --wanneer een postbusadres ingevuld dan dit gebruiken, anders vestigingsadres, 
 --wanneer contactpersoon aanwezig dan die naam in aanhef, anders 't.a.v. de directie'.
-SELECT S.name leverancier, IFNULL(CON.name, "t.a.v. de directie") aanhef, IF(S.p_address<>"", S.P_address, CONCAT(straat, " ", huisnr)) adres,
-IF(S.p_address<>"", S.P_postcode, S.postcode) postcode, IF(S.p_address<>"", CITP.name, CIT.name) stad,
-IF(S.p_address<>"", DISP.name, DIS.name) provincie
-FROM  mhl_suppliers S
-LEFT JOIN mhl_contacts CON ON S.id=CON.supplier_ID AND CON.department=3
-LEFT JOIN mhl_cities CIT ON S.city_ID=CIT.id
-LEFT JOIN mhl_communes COM ON CIT.commune_ID=COM.id
-LEFT JOIN mhl_districts DIS ON COM.district_ID=DIS.id
-LEFT JOIN mhl_cities CITP ON S.p_city_ID=CITP.id
-LEFT JOIN mhl_communes COMP ON CITP.commune_ID=COMP.id
-LEFT JOIN mhl_districts DISP ON COMP.district_ID=DISP.id
-WHERE postcode<>""
+SELECT S.name leverancier, IFNULL(CON.name, "t.a.v. de directie") aanhef, IF(sup.p_address<>"", sup.P_address, CONCAT(straat, " ", huisnr)) 
+adres, IF(sup.p_address<>"", sup.P_postcode, sup.postcode) postcode, IF(sup.p_address<>"", cit2.name, cit.name) stad,
+IF(sup.p_address<>"", dis2.name, dis.name) provincie
+FROM  mhl_suppliers sup
+LEFT JOIN mhl_contacts con ON sup.id=con.supplier_ID AND con.department=3
+LEFT JOIN mhl_cities cit ON sup.city_ID=cit.id
+LEFT JOIN mhl_communes com ON cit.commune_ID=com.id
+LEFT JOIN mhl_districts dis ON com.district_ID=dis.id
+LEFT JOIN mhl_cities cit2 ON sup.p_city_ID=cit2.id
+LEFT JOIN mhl_communes com2 ON cit2.commune_ID=com2.id
+LEFT JOIN mhl_districts dis2 ON com2.district_ID=dis2.id
+WHERE postcode <> ""
 ORDER BY provincie, stad, leverancier
-;
---
+++ relational-data-storage/opdracht-5/DS-opdracht-5.2.2.sql
@@ -0,0 +1,1 @@
+--5.2.2 Selecteer het aantal Gold, Silver, Bronze en Overige Suppliers per stad, aflopend gesorteerd op Gold, Silver, Bronze, Other.

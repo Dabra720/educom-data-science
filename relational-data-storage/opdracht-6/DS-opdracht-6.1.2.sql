32fb6e4b3b110c493744d441e67cc200c7bc907f
Daan Braas <daanbraas@gmail.com>
2023-04-04 16:59:35 +0200

    Opdracht 6.1 #7

--- relational-data-storage/opdracht-6/DS-opdracht-6.1.2.sql
+++ relational-data-storage/opdracht-6/DS-opdracht-6.1.2.sql
@@ -2,3 +2,26 @@
 --de totale hitcount en de gemiddelde hitcount van de betreffende gemeente, 
 --van die leveranciers die een hogere hitcount hebben dan dat gemiddelde, 
 --gesorteerd op het verschil (totaal ten opzichte van gemiddelde) in aflopende volgorde.
+
+-- SELECT com.name gemeente, s.name leverancier, SUM(hitcount) total_hitcount, AVG(hitcount) average_hitcount
+-- FROM mhl_suppliers s
+-- LEFT JOIN mhl_cities cit 
+-- ON cit.id=s.city_ID
+-- LEFT JOIN mhl_communes com
+-- ON com.id=cit.commune_ID
+-- LEFT JOIN mhl_hitcount h
+-- ON h.supplier_ID=s.id
+-- GROUP BY gemeente, leverancier
+-- ORDER BY gemeente
+SELECT com.name gemeente, SUM(hitcount) total, AVG(hitcount) average
+FROM mhl_suppliers s
+LEFT JOIN mhl_hitcount h
+ON h.supplier_ID=s.id
+LEFT JOIN mhl_cities cit 
+ON cit.id=s.city_ID
+LEFT JOIN mhl_communes com
+ON com.id=cit.commune_ID
+GROUP BY gemeente
+ORDER BY gemeente
+
+-- DEZE IS IN PROGRESS..

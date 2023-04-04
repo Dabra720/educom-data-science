7ada6aae0cc2705f616fc5bd9df1e0c910ce7ab3
Daan Braas <daanbraas@gmail.com>
2023-04-04 14:32:51 +0200

    Opdracht 4.1 #5

--- relational-data-storage/opdracht-4/DS-opdracht-4.1.8.sql
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.8.sql
@@ -1,2 +1,13 @@
 --4.1.8 Selecteer de plaatsnamen en id's van steden met dezelfde naam met een geldige gemeentenaam 
 --(toon ook de id en naam van de betreffende gemeenten), gesorteerd op plaatsnaam.
+SELECT c.name AS stad, c.id AS cid, c2.id AS cid2, c.commune_ID AS gid, c2.commune_ID AS gid2,
+com.name AS gemeente_1, com.name AS gemeente_2
+FROM mhl_cities c
+JOIN mhl_cities c2 
+ON c2.name=c.name
+JOIN mhl_communes com
+ON c.commune_ID=com.id
+JOIN mhl_communes com2 
+ON c2.commune_ID=com2.id
+WHERE c.id < C2.id
+ORDER BY c.name;

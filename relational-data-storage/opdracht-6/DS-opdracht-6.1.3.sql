f75f6d05d2489fe385f053ca6587f202eb862b8e
Daan Braas <daanbraas@gmail.com>
2023-04-05 15:20:31 +0200

    Opdracht 6.1 #7

--- relational-data-storage/opdracht-6/DS-opdracht-6.1.3.sql
+++ relational-data-storage/opdracht-6/DS-opdracht-6.1.3.sql
@@ -8,15 +8,14 @@ FROM mhl_rubrieken r1
 RIGHT JOIN mhl_rubrieken r2
 ON r2.parent=r1.id
 RIGHT JOIN (
-SELECT r.id, IFNULL(COUNT(s.id), 0) numsup
-FROM mhl_rubrieken r
-JOIN mhl_suppliers_mhl_rubriek_view sr
-ON sr.mhl_rubriek_view_ID=r.id
-JOIN mhl_suppliers s
-ON s.id=sr.mhl_suppliers_ID
-GROUP BY r.id
+SELECT r.id, r.name name, COUNT(mhl_suppliers_ID) numsup 
+FROM mhl_suppliers_mhl_rubriek_view sr
+JOIN mhl_rubrieken r
+ON r.id=sr.mhl_rubriek_view_ID
+GROUP BY mhl_rubriek_view_ID
 ) N
 ON N.id=r2.id
 ORDER BY name
+
 
 -- numsup aantallen wijken iets af, ik weet niet of dit aan mijn query ligt of aan de database.

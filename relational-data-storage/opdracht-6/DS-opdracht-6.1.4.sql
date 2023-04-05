3ade48d8395eb755aee317a32baa862b908ed7ee
Daan Braas <daanbraas@gmail.com>
2023-04-05 23:10:04 +0200

    Opdracht 6.1 #7

--- relational-data-storage/opdracht-6/DS-opdracht-6.1.4.sql
+++ relational-data-storage/opdracht-6/DS-opdracht-6.1.4.sql
@@ -7,3 +7,33 @@ RIGHT JOIN mhl_rubrieken r2
 ON r2.parent=r1.id
 ORDER BY name
 
+-- Vervolgens de hitcount per rubriek
+
+-- Hitcount per rubriek
+SELECT r.name name, SUM(hitcount) 
+FROM mhl_suppliers_mhl_rubriek_view sr
+LEFT JOIN mhl_hitcount hit
+ON hit.supplier_ID=sr.mhl_suppliers_ID
+RIGHT JOIN mhl_rubrieken r
+ON r.id=sr.mhl_rubriek_view_ID
+GROUP BY r.id
+
+-- Samenvoegen:
+
+SELECT CONCAT_WS('-', hoofd.name, sub.name) name,
+HIT.hitcount
+FROM mhl_rubrieken sub
+LEFT JOIN mhl_rubrieken hoofd
+ON hoofd.id=sub.parent
+LEFT JOIN (
+SELECT r.id, r.name name, IFNULL(SUM(hitcount), 'Geen hits') hitcount
+FROM mhl_suppliers_mhl_rubriek_view sr
+LEFT JOIN mhl_hitcount hit
+ON hit.supplier_ID=sr.mhl_suppliers_ID
+RIGHT JOIN mhl_rubrieken r
+ON r.id=sr.mhl_rubriek_view_ID
+GROUP BY r.id
+) HIT
+ON HIT.id=sub.id
+ORDER BY name
+

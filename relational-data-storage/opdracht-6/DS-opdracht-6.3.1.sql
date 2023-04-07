2b4801246a0df6b0f96255106fcd420465f95e0b
Daan Braas <daanbraas@gmail.com>
2023-04-07 17:09:15 +0200

    Opdracht 6 #7

--- relational-data-storage/opdracht-6/DS-opdracht-6.3.1.sql
+++ relational-data-storage/opdracht-6/DS-opdracht-6.3.1.sql
@@ -1,5 +1,8 @@
 --6.3.1 Selecteer alle plaatsnamen met uppercase van de eerste letter.
 SELECT name, 
-CONCAT(UCASE(LEFT(name, 1)), SUBSTRING(name, 2)) nice_name
+CASE
+WHEN name LIKE "'%" THEN CONCAT(LCASE(SUBSTRING(name, 1, 2)), UCASE(SUBSTRING(name, 3, 2)), SUBSTRING(name,5))
+ELSE CONCAT(UCASE(LEFT(name, 1)), SUBSTRING(name, 2)) 
+END nice_name
 FROM mhl_cities
 ORDER BY name
